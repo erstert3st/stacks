7809c68a957546c55a86239502be814edf18adbe
erstert3st <67842643+erstert3st@users.noreply.github.com>
2023-12-30 21:34:08 +0100

    Create commasetup.sh

---
+++ commasetup.sh
@@ -0,0 +1,14 @@
+setup 
+# UPDATE
+sudo apt update && sudo apt -y full-upgrade && sudo apt -y clean && sudo apt -y autoremove
+# Check 32/64 bit should be arch64
+uname -m
+#
+sudo apt install python3-full
+#docker 
+its updating so:
+https://docs.docker.com/engine/install/debian/
+
+#list hhd
+sudo blkid
+
